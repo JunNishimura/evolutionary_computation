c5d56d4629025cced50189ccf8b873e27e199fd0
JunNishimura <n.junjun0303@gmail.com>
2019-10-07 00:11:39 +0900

    add .gitignore

--- genetic algorithm/traveling salesperson problem/Individual.py
+++ genetic algorithm/traveling salesperson problem/Individual.py
@@ -1,3 +1,15 @@
 class Individual:
     def __init__(self):
         pass
+
+    # 評価
+    def evaluate(self):
+        pass
+
+    # 交叉
+    def crossover(self):
+        pass
+
+    # 突然変異
+    def mutate(self):
+        pass
--- genetic algorithm/traveling salesperson problem/Population.py
+++ genetic algorithm/traveling salesperson problem/Population.py
@@ -1,3 +1,15 @@
 class Population:
     def __init__(self):
         pass
+
+    # 世代交代
+    def alternate(self):
+        pass
+
+    # 結果表示
+    def printResult(self):
+        pass
+
+    # 親個体の選択
+    def select(self):
+        pass
